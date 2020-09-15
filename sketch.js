14efb1451fbd7ec939ca00476dd5506a7bcb580f
akshaya4 <70489515+akshaya4@users.noreply.github.com>
2020-09-15 16:45:03 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -4,6 +4,7 @@ const Engine = Matter.Engine;
 const World = Matter.World;
 const Bodies = Matter.Bodies;
 const Body = Matter.Body;
+const Render = Matter.Render;
 var dustbinObj, paperObject,groundObject;	
 var world;
 
